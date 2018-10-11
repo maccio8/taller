f692253010c175a5b7fff6511ad539a87bf9e466
santiago.perez <santiago.perez@AGESIC.GUB.UY>
2018-10-11 16:56:48 -0300

    Segundo codigo

---
+++ index.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<TITLE>Geodistribución de aplicaciones</TITLE>
+</HEAD>
+<BODY leftmargin=100 rightmargin=100>
+<img src="/img/logoEvento.png" style="max-width: 100%;  height:auto">
+<p><b><font size=6 color="#3155A5">Taller: Geodistribución de aplicaciones</font></b></p>
+<p><font size=5 color="#3155A5">Este sitio web fue desplegado 90% con herramientas de automatización que nos han permitido hacer uso de la infraestructura como código</font></p>
+<p> &nbsp;</p>
+<font size=4 color="#3155A5"><center>
+	<?php
+		echo "<p align=\"right\">El servidor que atendió el pedido es ";
+		echo "<b>";
+		echo  gethostname();
+		echo "</b></p>";
+	?>
+</center></font>
+</BODY>
+</HTML>
